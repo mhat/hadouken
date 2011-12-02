1c76bc8d4b5ed5737e9a4327ac1db575408c73a0
Matt Knopp <mknopp@yammer-inc.com>
2011-12-01 18:40:16 -0800

    add timestamp and timer

--- lib/hadouken/plan.rb
+++ lib/hadouken/plan.rb
@@ -3,10 +3,12 @@ class Hadouken::Plan
   attr_accessor :root
   attr_accessor :user
   attr_accessor :dry_run
+  attr_reader   :timestamp
   
   def initialize
-    @tasks  = Hadouken::Tasks.new
-    @groups = Hadouken::Groups.new
+    @tasks     = Hadouken::Tasks.new
+    @groups    = Hadouken::Groups.new
+    @timestamp = Time.now.utc.strftime("%Y%m%d%H%M%S")
   end
 
   def groups
--- lib/hadouken/runner.rb
+++ lib/hadouken/runner.rb
@@ -1,9 +1,13 @@
 class Hadouken::Runner
 
   def self.run
+    ts0  = Time.now
     plan = Hadouken::Plan.new
     yield plan
     Hadouken::Executor.run!(plan)
+    te0  = Time.now
+    Hadouken.logger.info "plan executed in %0.2f" % (te0 - ts0)
   end
+
 
 end
